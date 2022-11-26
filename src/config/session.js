4075c9b776253cbffeed4e69353178a130b0452b
Hung Ngo <hungngojan25@gmail.com>
2022-11-26 21:05:38 +0700

    done task https://hiereneko.atlassian.net/jira/software/projects/ISE422/boards/1?selectedIssue=ISE422-38

--- src/config/session.js
+++ src/config/session.js
@@ -3,8 +3,12 @@ module.exports = app =>{
     app.set('trust-proxy',1)
     app.use(session({
         secret: 'qwrtryt',
-        resave: false,
+        resave: false, // dat lai session cookie cho moi req
         saveUninitialized: true,
-        cookie: {secure: false}
+        cookie: {
+            secure: false,
+            maxAge: 900000,
+            httpOnly: true,
+        }
     }))
 }
